adedfa6ff82cb236535d854dec2716436cf578ee
Moh'd Alnooh <alnoohmohd@gmail.com>
2026-09-15 23:53:18 +0300

    create function  submissionService not sure if it is valid yet

--- src/services/submissionService/submissionService.js
+++ src/services/submissionService/submissionService.js
@@ -2,6 +2,17 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/audit-requests`;
 
 const create = async (requsetId, submissionData) => {
   try {
+    const response = await fetch(`${BASE_URL}/${requsetId}/submissions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(submissionData),
+    });
+
+    const data = await response.json();
+    return response.json();
   } catch (err) {
     console.log(err);
   }
