c348a1fc381b03fb0aab88b7d5c7762f361e9510
Moh'd Alnooh <alnoohmohd@gmail.com>
2026-09-15 23:47:12 +0300

    init commit

---
+++ src/services/submissionService/submissionService.js
@@ -0,0 +1,8 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/audit-requests`;
+
+const create = async (requsetId, submissionData) => {
+  try {
+  } catch (err) {
+    console.log(err);
+  }
+};
